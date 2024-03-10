19948831c527a94141fff59f2c8e2b2d34f0e0f7
YURII D <gkhelloworld@gmail.com>
2024-03-10 14:14:01 +0200

    add tags 2.1.1, 2.1.0

---
+++ wordpress_plugin/tags/2.1.0/index.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @version 2.1.0
+ */
+
+// Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit;
+} // Exit if accessed directly
---
+++ wordpress_plugin/tags/2.1.1/index.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @version 2.1.1
+ */
+
+// Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit;
+} // Exit if accessed directly
